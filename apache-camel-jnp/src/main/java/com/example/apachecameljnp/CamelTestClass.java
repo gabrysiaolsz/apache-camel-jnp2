6073ac0468e9b5888b38cb276498cea7f06314c0
dmkwis <43243733+dmkwis@users.noreply.github.com>
2021-05-25 17:28:45 +0200

    Add files via upload

---
+++ apache-camel-jnp/src/main/java/com/example/apachecameljnp/CamelTestClass.java
@@ -0,0 +1,12 @@
+package com.example.apachecameljnp;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CamelTestClass extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
